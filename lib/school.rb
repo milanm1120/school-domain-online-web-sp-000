34f8395778f3cbdb0fe55339fa961eb9f46250b6
Milan Mody <milanm1120@gmail.com>
2021-04-03 03:10:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,8 +7,8 @@ class School
    end
 
    def add_student(student_name, grade)
-     if roster[grade] = []
-       roster[grade] << student_name
+     if roster[grade]
+        roster[grade] << student_name
      else
        roster[grade] = [student_name]
      end
