1f39af3086a9dacab01d10b120b2b9f2523d0ddb
Milan Mody <milanm1120@gmail.com>
2021-04-03 03:16:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,21 +8,21 @@ class School
 
    def add_student(student_name, grade)
      if roster[grade]                     #creating a new key value (grade) for the hash
-        roster[grade] << student_name     #pushing the new value into the key 
+        roster[grade] << student_name     #pushing the new value into the key
      else
        roster[grade] = [student_name]
      end
    end
-   #
-  #  def grade(grade)
-  #    @roster[grade]
-  #  end
-   #
-  #  def sort
-  #    sorted={}
-  #    roster.each do |grade, name|
-  #      sorted[grade]= name.sort
-  #    end
-  #    sorted
-  #  end
+
+   def grade(grade)
+     @roster[grade]                     #returning array of values set to "key value"
+   end
+
+   def sort
+     sorted={}
+     roster.each do |grade, name|
+       sorted[grade]= name.sort
+     end
+     sorted
+   end
  end
